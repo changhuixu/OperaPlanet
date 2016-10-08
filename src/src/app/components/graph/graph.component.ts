f4359295ae55c0a1a57377dc49af438bd855bd13
Mostafa Abdelraouf <mostafa.mohmmed@gmail.com>
2016-10-08 12:24:26 -0500

    Initial strcuture

---
+++ src/src/app/components/graph/graph.component.ts
@@ -0,0 +1,72 @@
+import {Component, ViewChild} from '@angular/core';
+let vis =  require('vis');
+
+@Component({
+  selector: 'graph-component',
+  template: '<div #container></div>',
+  styles : [`
+      :host {
+          width: 250px;
+          height: 250px;
+      }`]
+})
+export class GraphComponent {
+
+  @ViewChild('container') domContainer;
+
+  private nodeDataset = null;
+  private edgeDataset = null;
+  private graph = null;
+
+  ngOnInit() {
+    // create an array with nodesDS
+    this.nodeDataset = new vis.DataSet();
+    this.edgeDataset = new vis.DataSet();
+
+    let data = {
+      nodes: this.nodeDataset,
+      edges: this.edgeDataset
+    };
+
+    var options = {
+      nodes: {
+        scaling: {
+          min: 16,
+          max: 32
+        },
+        font: {
+          size: 16,
+          face: 'Droid Arabic Naskh',
+          strokeWidth: 1
+        }
+      },
+      edges: {
+        color: {
+          'color': '#aa0000',
+          'hover': '#00aa00',
+          'highlight': '#0000aa'
+        },
+        font: {
+          size: 14,
+          face: 'Droid Arabic Naskh'
+        },
+        selfReferenceSize: 75,
+        length: 300,
+        width: 0.1
+      },
+      interaction:{
+        hover:true,
+        navigationButtons: false
+      },
+      physics:{
+        barnesHut:{
+          gravitationalConstant:-15000,
+          avoidOverlap:1
+        },
+        stabilization: {iterations:150}
+      },
+    };
+
+    this.graph  = new vis.Network(this.domContainer.nativeElement, data, options);
+  }
+}
---
+++ src/src/app/components/window/window.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'opera-item',
+  templateUrl: './window.component.html',
+  styleUrls: ['./window.component.css']
+})
+export class OperaComponent {
+  title = 'Opera Component';
+}
