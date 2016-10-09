3fbdbc680398a9fa907c45722af54c06a20bc65d
Mostafa Abdelraouf <mostafa.mohmmed@gmail.com>
2016-10-09 08:34:24 -0500

    Moved data to seaprate files

---
+++ src/src/app/data/ntm_1.ts
@@ -0,0 +1,47 @@
+export var ntm_1 = {
+    "id": "node_4",
+    "label": "MUSIC",
+    "group": "ntm",
+    "data": {
+        "@id": "_:ub42bL39C22",
+        "Organization": "Naxos Digital Services Ltd",
+        "bf:InstanceTitle": "(NTM_4) MOZART : Marriage of Figaro",
+        "bf:Note": [
+            {
+                "bf:noteType": "Access condition",
+                "rdfs:label": "Access restricted to subscribers."
+            },
+            {
+                "bf:noteType": "Mode of Access",
+                "rdfs:label": "Mode of Access: World Wide Web browser."
+            }
+        ],
+        "bf:WorkTitle": "Naxos Music Library.",
+        "bf:Person": {
+            "@id": "http://id.loc.gov/rwo/agents/n80022788",
+            "@type": "bf:Person",
+            "rdfs:label": "Mozart, Wolfgang Amadeus, 1756-1791."
+        },
+        "bf:carrier": {
+            "@id": "http://id.loc.gov/vocabulary/carriers/cr"
+        },
+        "bf:extent": "1 online resource (82 streaming audio files)",
+        "bf:genreForm": {
+            "@id": "_:ub42bL31C15"
+        },
+        "bf:illustrativeContent": "digital",
+        "bf:instanceOf": {
+            "@id": "http://bibframe.org/resources/SEj1470609719/990057789670202771"
+        },
+        "bf:media": {
+            "@id": "http://id.loc.gov/vocabulary/mediaTypes/c"
+        },
+        "bf:provisionActivity": {
+            "@id": "_:ub42bL37C26"
+        },
+        "bf:provisionActivityStatement": "Hong Kong : Naxos Digital Services Ltd., 2007",
+        "bf:seriesStatement": "Chandos",
+        "bf:soundContent": "Sound on medium"
+
+    }
+};
