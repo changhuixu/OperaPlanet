16bd74249f394262fe7bd682d68a38965effe617
Mostafa Abdelraouf <mostafa.mohmmed@gmail.com>
2016-10-09 08:34:24 -0500

    Moved data to seaprate files

---
+++ src/src/app/data/txt_1.ts
@@ -0,0 +1,109 @@
+export var txt_1 =  {
+    "id": "node_5",
+    "label": "Libretto",
+    "group": "txt",
+    "data": {
+        "@id": "_:ub42bL39C22",
+        "Organization": "Naxos Digital Services Ltd",
+        "bf:InstanceTitle": "MOZART : Marriage of Figaro",
+        "bf:Note": [
+            {
+                "bf:noteType": "Access condition",
+                "rdfs:label": "Access restricted to subscribers."
+            },
+            {
+                "bf:noteType": "Mode of Access",
+                "rdfs:label": "Mode of Access: World Wide Web browser."
+            }
+        ],
+        "bf:WorkTitle": "Naxos Music Library.",
+        "bf:Content": {
+            "@id": "http://id.loc.gov/vocabulary/contentTypes/prm",
+            "rdfs:label": "performed music"
+        },
+        "bf:Organization": {
+            "@id": "http://id.loc.gov/authorities/names/n78094508",
+            "rdfs:label": "Geoffrey Mitchell Choir"
+        },
+        "bf:Person": [
+            {
+                "@id": "http://id.loc.gov/rwo/agents/n80022788",
+                "@type": "bf:Person",
+                "rdfs:label": "Mozart, Wolfgang Amadeus, 1756-1791."
+            },
+            {
+                "@id": "http://id.loc.gov/rwo/agents/n80125207",
+                "@type": "bf:Person",
+                "rdfs:label": "Beaumarchais, Pierre Augustin Caron de, 1732-1799 "
+            },
+            {
+                "@id": "http://id.loc.gov/rwo/agents/n80057191",
+                "@type": "bf:Person",
+                "rdfs:label": "Da Ponte, Lorenzo, 1749-1838"
+            }
+        ],
+        "bf:carrier": {
+            "@id": "http://id.loc.gov/vocabulary/carriers/cr"
+        },
+        "bf:extent": "1 online resource (82 streaming audio files)",
+        "bf:genreForm": {
+            "@id": "_:ub42bL31C15"
+        },
+        "bf:illustrativeContent": "digital",
+        "bf:instanceOf": {
+            "@id": "http://bibframe.org/resources/SEj1470609719/990057789670202771"
+        },
+        "bf:media": {
+            "@id": "http://id.loc.gov/vocabulary/mediaTypes/c"
+        },
+        "bf:provisionActivity": {
+            "@id": "_:ub42bL37C26"
+        },
+        "bf:provisionActivityStatement": "Hong Kong : Naxos Digital Services Ltd., 2007",
+        "bf:seriesStatement": "Chandos",
+        "bf:soundContent": "Sound on medium",
+        "relatedPersons":[
+            {
+                "@id": "http://id.loc.gov/rwo/agents/n92027115",
+                "@type": "bf:Person",
+                "rdfs:label": "Kale, Stuart"
+            },
+            {
+                "@id": "http://id.loc.gov/authorities/names/no2006019760",
+                "@type": "bf:Person",
+                "rdfs:label": "Bonner, Yvette, 1973- "
+            },
+            {
+                "@id": "http://id.loc.gov/rwo/agents/no96042093",
+                "@type": "bf:Person",
+                "rdfs:label": "Parry, David, 1949- "
+            },
+            {
+                "@id": "http://id.loc.gov/rwo/agents/n80022788",
+                "@type": "bf:Person",
+                "rdfs:label": "Mozart, Wolfgang Amadeus, 1756-1791"
+            },
+            {
+                "@id": "http://id.loc.gov/rwo/agents/n88627414",
+                "@type": "bf:Person",
+                "rdfs:label": "Montague, Diana"
+            },
+            {
+                "@id": "http://id.loc.gov/rwo/agents/no2001047614",
+                "@type": "bf:Person",
+                "rdfs:label": "McCafferty, Frances"
+            }
+        ],
+        "relatedPlaces":[
+            {
+                "@id": "_:ub42bL41C22",
+                "@type": "bf:Place",
+                "rdfs:label": "Hong Kong "
+            }
+        ],
+        "InfoHawkPlus":{
+            "@id": "http://search.lib.uiowa.edu/01IOWA:default_scope:01IOWA_ALMA51553243030002771",
+            "rdfs:comment": "Connect to the University of Iowa Libraries' Online Catalog - InfoHawk+"
+        }
+    }
+};
